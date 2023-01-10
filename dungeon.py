f691dcf25e25137d9028b91aad4df8e825520cb3
Blue Tyson <bluetyson@users.noreply.github.com>
2023-01-11 09:54:48 +1030

    Room result for L

--- dungeon.py
+++ dungeon.py
@@ -190,6 +190,60 @@ def check_action(pc_dict, coord):
                             else:
                                 break
 
+                if e_dict['beyond'] == 'Room':
+                    '''
+                        lr = roll_dict(1,2)
+                        shape_dict['size'] = [2,2]
+                        shape_dict['size'] = [4,4]
+                        shape_dict['size'] = [1,2]
+                        shape_dict['size'] = [2,4]
+                        shape_dict['size'] = [4,6]
+                        shape_dict['size'] = [3,4]
+                        
+                        others
+                        shape_dict['size'] = [1,1]
+                        shape_dict['size'] = [3,3]
+                        shape_dict['size'] = [2,3]
+                        shape_dict['size'] = [2,3]
+                        shape_dict['size'] = [3,5]
+                
+                        shape_dict = fancy_shape()
+                    '''
+                    #want those we randomly position lr
+                    new_coord = coord
+                    shape_dict = room(coord, C='R')  ## different type to get slightly different table
+                    #each room part check for inside
+
+                    print("ROOM SHAPE:",shape_dict)
+                    ## do simple version first of x directions and y directions of rectangular
+                    if shape_dict['shape'] == 'R':
+                        print("rectangular")
+                        #H x W
+                        #position based on size
+                        adjust = 0
+                        if shape_dict['size'][1] % 2 == 0:
+                            lr = roll_dice(1,2)
+                            if lr == 1:
+                                adjust = -1
+                            else:
+                                adjust = 1
+
+                        #if shape_dict['size'][1] == 2:
+                        for j in range(shape_dict['size'][1]):
+                            for i in range(shape_dict['size'][0]):
+                                will_fit = in_dungeon((coord[0] + i + adjust,coord[1]+j+1,coord[2]))
+                                if not will_fit:                
+                                    dungeon[(coord[0] + i + adjust,coord[1]+j+1,coord[2])] = {}
+                                    dungeon[(coord[0] + i + adjust,coord[1]+j+1,coord[2])]['fill'] = 'R'
+                                else:
+                                    break
+                        
+                    else:
+                        pass
+                        #fancy shape/size not yet implemented
+
+
+
 
         elif 'R' in e_dict:
             if e_dict['type'] == 'N':
