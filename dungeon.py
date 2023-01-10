82459accce328c9944231a6f9e60aff3dc3641db
Blue Tyson <bluetyson@users.noreply.github.com>
2023-01-10 14:32:27 +1030

    room size function

--- dungeon.py
+++ dungeon.py
@@ -74,8 +74,6 @@ def random_check():
 
     print("RANDOM_CHECK",pc, pc_dict['direction'])
 
-
-
     return pc_dict
 
 def check_action(pc_dict, coord):
@@ -374,8 +372,40 @@ def turn(coord):
     return t_dict
 
 
-def room(coord):
-    pass
+def room(coord, size="C"):
+    r = roll_dice(1,20)
+    room_size = [0,0]
+    if r <= 2:
+        if size="C":
+            room_size = [2,2]
+        else:
+            room_size = [1,1]
+    if r >= 3 and r<=4:
+        room_size = [2,2]
+    if r >= 5 and r<=6:
+        room_size = [3,3]
+    if r >= 7 and r<=8:
+        room_size = [4,4]
+    if r >= 9 and r <=10:
+        if size="C":
+            room_size = [2,3]
+        else:
+            room_size = [1,2]
+    if r >= 11 and r<=13:
+        room_size = [2,3]
+    if r >= 14 and r <=15:
+        if size="C":
+            room_size = [3,5]
+        else:
+            room_size = [2,4]
+    if r >= 16 and r <=17:
+        if size="C":
+            room_size = [4,6]
+        else:
+            room_size = [3,4]
+    if r >= 18:
+            room_size = [-1,-1]
+    return room_size
 
 #not implemented yet
 def width():
