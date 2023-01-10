fca841173b193c3663338c6fb7d7cdb0dc953adc
Blue Tyson <bluetyson@users.noreply.github.com>
2023-01-10 19:36:15 +1030

    put in gray for dead end

--- dungeon.py
+++ dungeon.py
@@ -1002,6 +1002,8 @@ with open('dungeon.html','w') as f:
                 strdata = '<td>' + chararray[i,j,0] + '</td>'
             elif chararray[i,j,0] == 'R':
                 strdata = '<td class="blue_background">' + chararray[i,j,0] + '</td>'
+            elif chararray[i,j,0] == 'D':
+                strdata = '<td class="gray_background">' + chararray[i,j,0] + '</td>'
 
             else:
                 strdata = '<td class="red_background">' + chararray[i,j,0] + '</td>'
