1f09c8114d7378af8da91da142a50f4608047ecc
Blue Tyson <bluetyson@users.noreply.github.com>
2023-01-10 13:17:28 +1030

    map widths

--- dungeon.py
+++ dungeon.py
@@ -525,7 +525,8 @@ chararray[0,0,0] = 'S'
 for key in dungeon:
     print(key)
     if 'fill' in dungeon[key]:
-        chararray[[key][0],key[1],key[2]] = dungeon[key]['fill']
+        #chararray[[key][0],key[1],key[2]] = dungeon[key]['fill']
+        chararray[[key][0]+xwidth,key[1]+ywidth,key[2]+zwidth] = dungeon[key]['fill']
 chararray[0,0,0] = 'S'        
 
 print("SHAPE",chararray.shape)
