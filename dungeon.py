9d2f7a5c4b92a251538888b7df0e3086500494bc
Blue Tyson <bluetyson@users.noreply.github.com>
2023-01-10 16:07:02 +1030

    a7

--- dungeon.py
+++ dungeon.py
@@ -618,7 +618,7 @@ def wet_magic():
         a = roll_dice(1,20)
         if a <= 6:
             wet_magic_dict['alignment'] = 'LG'
-        elif a >= 7: and a <= 9
+        elif a >= 7 and a <= 9
             wet_magic_dict['alignment'] = 'LE'
         elif a >= 10: and a <= 12
             wet_magic_dict['alignment'] = 'CG'
