61e0380d269a4ababdcf4461bbe9917325997c97
Blue Tyson <bluetyson@users.noreply.github.com>
2023-01-12 19:45:49 +1030

    int ARGV

--- dungeon.py
+++ dungeon.py
@@ -1912,7 +1912,7 @@ print (testmonster() + ' ' + testtreasure())
 PERIODIC_CHECKS = 1  #number of rolls to make before stopping algorithm  #don't count first one down
 
 if len(ARGV) > 0:
-    if ARGV[1] > 1:
+    if int(ARGV[1]) > 1:
         PERIODIC_CHECKS = ARGV[1]
 #make this a script argument
 START_LEVEL = 0
