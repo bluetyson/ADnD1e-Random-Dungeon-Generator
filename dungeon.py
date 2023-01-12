1cb49a187dcd97bf3812ec951fa73a6d927b365d
Blue Tyson <bluetyson@users.noreply.github.com>
2023-01-12 11:17:30 +1030

    random encounter coord fix

--- dungeon.py
+++ dungeon.py
@@ -797,10 +797,10 @@ def check_action(pc_dict, coord):
 
         will_fit = in_dungeon((coord[0],coord[1]+1,coord[2])) #square for monster
         if not will_fit:                                                
-            dungeon[(coord[0],coord[1]+1,coord[2])+1] = {}
-            dungeon[(coord[0],coord[1]+1,coord[2])+1]['fill'] = 'wm'
+            dungeon[(coord[0],coord[1]+1,coord[2]1)+] = {}
+            dungeon[(coord[0],coord[1]+1,coord[2]+1)]['fill'] = 'wm'
         else:
-            dungeon[(coord[0],coord[1]+1,coord[2])+1]['fill'] = dungeon[(coord[0],coord[1]+1,coord[2])+1]['fill'] + 'wm'
+            dungeon[(coord[0],coord[1]+1,coord[2]+1)]['fill'] = dungeon[(coord[0],coord[1]+1,coord[2]+1)]['fill'] + 'wm'
 
     return new_coord
 
