707f851c8295ce60c8ee12080cfb223373277932
Blue Tyson <bluetyson@users.noreply.github.com>
2023-01-15 12:26:45 +1030

    check if any rooms

--- dungeon.py
+++ dungeon.py
@@ -2742,8 +2742,9 @@ for down in range(zwidth-1):
             #f.write('</TR>')
         f.write('</table>')
 
-        for room in room_stack['shape_dict']:
-            f.write(room_stack['shape_dict'][room])
+        if 'shape_dict' in room_stack:
+            for room in room_stack['shape_dict']:
+                f.write(room_stack['shape_dict'][room])
 
         f.write(strend)
 
