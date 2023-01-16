be649b86ecf42b719f02d1c40297dab0a576f348
Blue Tyson <bluetyson@users.noreply.github.com>
2023-01-16 18:33:40 +1030

    brackets

--- dungeon.py
+++ dungeon.py
@@ -3550,7 +3550,7 @@ for down in range(zwidth-1):
 
 
                     if 'sd' in usestr:  #number and d
-                        dead_end = dead_end_dict(i+xmin,j+ymin,0 - down -1)
+                        dead_end = dead_end_dict[(i+xmin,j+ymin,0 - down -1)]
 
                         print("found Dead End Secret door")
                         if dead_end == 'ymax':
