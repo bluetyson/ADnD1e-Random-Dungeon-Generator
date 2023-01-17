486f02980179d215a770f98728eb5cd50ce7f54c
Blue Tyson <bluetyson@users.noreply.github.com>
2023-01-17 16:09:55 +1030

    water DICT

--- dungeon.py
+++ dungeon.py
@@ -3883,6 +3883,6 @@ with open('downlist.pkl','rb') as fd:
 
 print("\nFINAL ROOM STACK",room_stack)   
     
-print("\nERROR LOG",error_dict,water_dict)
+print("\nERROR LOG",error_dict,"WATER_DICT:",water_dict)
 
 #print("\nWATER LOG",
