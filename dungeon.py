c4647a435d7efa8eee7b67577a8b89c51a622f9e
Blue Tyson <bluetyson@users.noreply.github.com>
2023-01-17 17:59:23 +1030

    )

--- dungeon.py
+++ dungeon.py
@@ -420,7 +420,7 @@ def check_action(pc_dict, coord, room_stack):
                     if not will_fit:
                         #dungeon[(coord[0],coord[1]+1,coord[2])] = {}
                         #handle very first cool being cooridor door creation
-                        if (coord[0],coord[1],coord[2] not in dungeon:
+                        if (coord[0],coord[1],coord[2]) not in dungeon:
                             dungeon[(coord[0],coord[1],coord[2])] = {}}
                         dungeon[(coord[0],coord[1],coord[2])]['fill'] = 'Cd'
                         new_coord = (coord[0],coord[1]+1,coord[2])
