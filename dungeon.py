b16fc65e9ff54dab10288ab31af19c071917f880
Blue Tyson <bluetyson@users.noreply.github.com>
2023-01-17 17:59:47 +1030

    {}

--- dungeon.py
+++ dungeon.py
@@ -421,7 +421,7 @@ def check_action(pc_dict, coord, room_stack):
                         #dungeon[(coord[0],coord[1]+1,coord[2])] = {}
                         #handle very first cool being cooridor door creation
                         if (coord[0],coord[1],coord[2]) not in dungeon:
-                            dungeon[(coord[0],coord[1],coord[2])] = {}}
+                            dungeon[(coord[0],coord[1],coord[2])] = {}
                         dungeon[(coord[0],coord[1],coord[2])]['fill'] = 'Cd'
                         new_coord = (coord[0],coord[1]+1,coord[2])
                         # 10 x 10 room need to check contents!
