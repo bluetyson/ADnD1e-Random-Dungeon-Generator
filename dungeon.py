d4eb6e034050f02ba0e047a87edac30848647731
Blue Tyson <bluetyson@users.noreply.github.com>
2023-01-19 08:43:54 +1030

    put widths in

--- dungeon.py
+++ dungeon.py
@@ -578,27 +578,27 @@ def check_action(pc_dict, coord, room_stack):
         s_dict = side(coord)
         print("S_DICT:",s_dict)
         if s_dict['direction'] == 'L90':
-            new_coord = passage_make(coord,xmod=-1,xloop=-1)    
+            new_coord = passage_make(coord,xmod=-1,xloop=-1,ywidth=1)    
 
         elif s_dict['direction'] == 'R90':
-            new_coord = passage_make(coord,xmod=1,xloop=1)    
+            new_coord = passage_make(coord,xmod=1,xloop=1,ywidth=1)    
 
         elif s_dict['direction'] == 'L45':
-            new_coord = passage_make(coord,xmod=-1,xloop=-1,ymod=1,yloop=1)    
+            new_coord = passage_make(coord,xmod=-1,xloop=-1,ymod=1,yloop=1,xwidth=1)    
 
         elif s_dict['direction'] == 'R45':
-            new_coord = passage_make(coord,xmod=1,xloop=1,ymod=1,yloop=1)    
+            new_coord = passage_make(coord,xmod=1,xloop=1,ymod=1,yloop=1,xwidth=1)    
 
         elif s_dict['direction'] == 'L135':
-            new_coord = passage_make(coord,xmod=-1,xloop=-1,ymod=-1,yloop=-1)    
+            new_coord = passage_make(coord,xmod=-1,xloop=-1,ymod=-1,yloop=-1,xwidth=1)    
 
         elif s_dict['direction'] == 'R135':
-            new_coord = passage_make(coord,xmod=1,xloop=1,ymod=-1,yloop=-1)    
+            new_coord = passage_make(coord,xmod=1,xloop=1,ymod=-1,yloop=-1,xwidth=1)    
 
         elif s_dict['direction'] == 'T':
             which_way = roll_dice(1,2)
-            new_coord_left = passage_make(coord,xmod=-1,xloop=-1)    
-            new_coord_right = passage_make(coord,xmod=1,xloop=1)    
+            new_coord_left = passage_make(coord,xmod=-1,xloop=-1,ywidth=1)    
+            new_coord_right = passage_make(coord,xmod=1,xloop=1,ywidth=1)    
 
             if which_way == 1:
                 new_coord = new_coord_left
@@ -607,8 +607,8 @@ def check_action(pc_dict, coord, room_stack):
 
         elif s_dict['direction'] == 'Y':
             which_way = roll_dice(1,2)  #got to move this up
-            newcoord_left = passage_make(coord,xmod=-1,xloop=-1,ymod=1,yloop=1)    
-            new_coord_right = passage_make(coord,xmod=1,xloop=1,ymod=1,yloop=1)    
+            newcoord_left = passage_make(coord,xmod=-1,xloop=-1,ymod=1,yloop=1,xwidth=1)    
+            new_coord_right = passage_make(coord,xmod=1,xloop=1,ymod=1,yloop=1,xwidth=1)    
             if which_way == 1:
                 new_coord = newcoord_left
             else:
@@ -616,9 +616,9 @@ def check_action(pc_dict, coord, room_stack):
 
         elif s_dict['direction'] == 'P': #plus
             which_way = roll_dice(1,3)
-            new_coord_left = passage_make(coord,xmod=-1,xloop=-1)    
-            new_coord_right = passage_make(coord,xmod=1,xloop=1)    
-            new_coord_ahead = passage_make(coord,ymod=1,yloop=1)    
+            new_coord_left = passage_make(coord,xmod=-1,xloop=-1,ywidth=1)    
+            new_coord_right = passage_make(coord,xmod=1,xloop=1,ywidth=1)    
+            new_coord_ahead = passage_make(coord,ymod=1,yloop=1,xwidth=1)    
 
             if which_way == 1:
                 new_coord = new_coord_left
@@ -630,11 +630,11 @@ def check_action(pc_dict, coord, room_stack):
         else:
         #s_dict['direction'] == 'X': 45 and 135
             which_way = roll_dice(1,4) ##got to move this up
-            newcoord_left = passage_make(coord,xmod=-1,xloop=-1,ymod=1,yloop=1)    
-            new_coord_right = passage_make(coord,xmod=1,xloop=1,ymod=1,yloop=1) 
+            newcoord_left = passage_make(coord,xmod=-1,xloop=-1,ymod=1,yloop=1,xwidth=1)    
+            new_coord_right = passage_make(coord,xmod=1,xloop=1,ymod=1,yloop=1,xwidth=1) 
             
-            new_coord_left_back = passage_make(coord,xmod=-1,xloop=-1,ymod=-1,yloop=-1)    
-            new_coord_right_back = passage_make(coord,xmod=1,xloop=1,ymod=-1,yloop=-1)                   
+            new_coord_left_back = passage_make(coord,xmod=-1,xloop=-1,ymod=-1,yloop=-1,xwidth=1)    
+            new_coord_right_back = passage_make(coord,xmod=1,xloop=1,ymod=-1,yloop=-1,xwidth=1)                   
 
             if which_way == 1:
                 new_coord = new_coord_left
