28f72238fc7f42987aaeeba170fa08b453645412
Blue Tyson <bluetyson@users.noreply.github.com>
2023-01-19 18:32:15 +1030

    wm overwrite fill test

--- dungeon.py
+++ dungeon.py
@@ -704,7 +704,9 @@ def check_action(pc_dict, coord, room_stack):
             dungeon[(coord[0],coord[1]+1,coord[2])]['fill'] = 'wm'
         else:
             print('wm filled up check:',dungeon[(coord[0],coord[1]+1,coord[2])])
-            dungeon[(coord[0],coord[1]+1,coord[2])]['fill'] = 'wm' #get this to work
+            #dungeon[(coord[0],coord[1]+1,coord[2])]['fill'] = 'wm' #get this to work
+            # do fill test to not write over other fills like 'monsters
+            dungeon[(coord[0],coord[1]+1,coord[2])]['fill'] = dungeon[(coord[0],coord[1]+1,coord[2])]['fill'] + 'wm' #get this to work'wm' #get this to work
             #dungeon[(coord[0],coord[1]+1,coord[2])]['fill'] = dungeon[(coord[0],coord[1]+1,coord[2])]['fill'] + 'wm'
             print('wm key check',dungeon[(coord[0],coord[1]+1,coord[2])])
 
