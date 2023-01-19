342b82b4162ca779b87cc5d59420615d43218e42
Blue Tyson <bluetyson@users.noreply.github.com>
2023-01-19 14:21:10 +1030

    L A

--- test_dungeon.py
+++ test_dungeon.py
@@ -214,7 +214,7 @@ def check_action(pc_dict, coord, room_stack):
         e_dict['beyond'] = '4BA'
         e_dict['beyond'] = '4AB'
         e_dict['beyond'] = 'A'
-        e_dict['direction'] = 'R'
+        e_dict['direction'] = 'L'
 
         print("EDICT:",e_dict)
 
