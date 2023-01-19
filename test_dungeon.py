79170f9ee188515fa298bcba14d573d70dcb8d19
Blue Tyson <bluetyson@users.noreply.github.com>
2023-01-19 10:35:20 +1030

    ,

--- test_dungeon.py
+++ test_dungeon.py
@@ -180,7 +180,7 @@ def passage_make(coord, loop=3,xmod=0,ymod=0,zmod=0,xloop=0,yloop=1,zloop=0,xwid
             for y in range(loop):
                 
                 #checkcoord = (coord[0]+xmod+xloop*loop+xwidth*w,coord[1]+ymod+yloop*loop+ywidth*w,coord[2]+zmod+zloop*loop)
-                print("COORD BEFORE:"(coord[0]+xmod+xloop*loop+xwidth*w,coord[1]+ymod+yloop*loop+ywidth*w,coord[2]+zmod+zloop*loop))
+                print("COORD BEFORE:",(coord[0]+xmod+xloop*loop+xwidth*w,coord[1]+ymod+yloop*loop+ywidth*w,coord[2]+zmod+zloop*loop))
                 will_fit = in_dungeon((coord[0]+xmod+xloop*loop+xwidth*w,coord[1]+ymod+yloop*loop+ywidth*w,coord[2]+zmod+zloop*loop))
                 print("width:",w,"loop:","willfit:",will_fit,(coord[0]+xmod+xloop*y,coord[1]+ymod+yloop*y,coord[2]+zmod+zloop*y))
                 if not will_fit:
