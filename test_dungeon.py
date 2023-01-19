a9bda1b03492b3bdb078c3228d4a7b753f8864b5
Blue Tyson <bluetyson@users.noreply.github.com>
2023-01-19 12:53:07 +1030

    refactor L beyond P

--- test_dungeon.py
+++ test_dungeon.py
@@ -208,34 +208,48 @@ def check_action(pc_dict, coord, room_stack):
 
         new_coord = coord
         e_dict = exit(coord)
-
         #test L parallel
         e_dict['beyond'] = 'P'
         e_dict['direction'] = 'L'
-
         print("EDICT:",e_dict)
+        if 1 == 2:
+            if e_dict['direction'] == 'L':
+                if e_dict['type'] == 'N':
+                    exit_stack[(coord[0]-1,coord[1],coord[2])] = {}
+                    if e_dict['beyond'] == 'P':
+                        new_coord = passage_make_full(coord, xmod=-1,ymod=-1,yloop=1,xwidth=1)
+                        new_coord = passage_make_full(coord, xmod=-1,ymod=1,yloop=1,xwidth=1)
+
+                    if e_dict['beyond'] == 'A':              
+                        d = roll_dice(1,20)              
+                        if d >=3 and d >= 5:
+                            will_fit = in_dungeon((coord[0]-1,coord[1],coord[2]))
+                            if not will_fit:
+                                exit_stack[(coord[0]-1,coord[1],coord[2])] = {}
+                        else:
+                            #30m passage that direction
+                            new_coord = passage_make_full(coord, xmod=-1,xloop=-1, ywidth=1)
+
+                    if e_dict['beyond'] == '4AB':   ##45 A
+                        which_way = roll_dice(1,2)           
+                        if which_way == 1:  #corridor left
+                            new_coord = passage_make_full(coord, xmod=-1,xloop=-1,ymod=1,yloop=1,ywidth=1)
+                        else:
+                            new_coord = passage_make_full(coord, xmod=1,xloop=1,ymod=1,yloop=1,ywidth=1)
+
+                    if e_dict['beyond'] == '4BA':   ##45 A
+                        which_way = roll_dice(1,2)           
+                        if which_way == 1:  #corridor left
+                            new_coord = passage_make_full(coord, xmod=-1,xloop=-1,ymod=-1,yloop=-1,ywidth=1)
+                        else:
+                            new_coord = passage_make_full(coord, xmod=1,xloop=1,ymod=-1,yloop=-1,ywidth=1)
+
         if e_dict['direction'] == 'L':
             if e_dict['type'] == 'N':
                 exit_stack[(coord[0]-1,coord[1],coord[2])] = {}
                 if e_dict['beyond'] == 'P':
-                    for x in range(3):
-                        will_fit = in_dungeon((coord[0]-1,coord[1]+x-1,coord[2]))
-                        if not will_fit:
-                            dungeon[(coord[0]-1,coord[1]+x-1,coord[2])] = {}
-                            if x != 0:
-                                dungeon[(coord[0]-1,coord[1]+x-1,coord[2])]['fill'] = 'C'
-                            else:
-                                dungeon[(coord[0]-1,coord[1]+x-1,coord[2])]['fill'] = 'Cd'
-                            new_coord = (coord[0]-1,coord[1]+x-1,coord[2])
-                    for x in range(3):
-                        will_fit = in_dungeon((coord[0]-1,coord[1]+x+1,coord[2]))
-                        if not will_fit:
-                            dungeon[(coord[0]-1,coord[1]+x+1,coord[2])] = {}
-                            if x != 0:
-                                dungeon[(coord[0]-1,coord[1]+x+1,coord[2])]['fill'] = 'C'
-                            else:
-                                dungeon[(coord[0]-1,coord[1]+x+1,coord[2])]['fill'] = 'Cd'
-                            new_coord = (coord[0]-1,coord[1]+x+1,coord[2])
+                    new_coord = passage_make_full(coord, xmod=-1,ymod=-1,yloop=1,xwidth=1)
+                    new_coord = passage_make_full(coord, xmod=-1,ymod=1,yloop=1,xwidth=1)
 
                 if e_dict['beyond'] == 'A':              
                     d = roll_dice(1,20)              
