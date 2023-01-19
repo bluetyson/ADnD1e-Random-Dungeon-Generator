4ade563ae1457fdb0f30372f61dcea589b206b06
Blue Tyson <bluetyson@users.noreply.github.com>
2023-01-19 19:04:04 +1030

    wm fill check

--- dungeon.py
+++ dungeon.py
@@ -706,8 +706,10 @@ def check_action(pc_dict, coord, room_stack):
             print('wm filled up check:',dungeon[(coord[0],coord[1]+1,coord[2])])
             #dungeon[(coord[0],coord[1]+1,coord[2])]['fill'] = 'wm' #get this to work
             # do fill test to not write over other fills like 'monsters
-            dungeon[(coord[0],coord[1]+1,coord[2])]['fill'] = dungeon[(coord[0],coord[1]+1,coord[2])]['fill'] + 'wm' #get this to work'wm' #get this to work
-            #dungeon[(coord[0],coord[1]+1,coord[2])]['fill'] = dungeon[(coord[0],coord[1]+1,coord[2])]['fill'] + 'wm'
+            if ['fill'] in dungeon[(coord[0],coord[1]+1,coord[2])]:
+                dungeon[(coord[0],coord[1]+1,coord[2])]['fill'] = dungeon[(coord[0],coord[1]+1,coord[2])]['fill'] + 'wm' #get this to work'wm' #get this to work
+            else:
+                dungeon[(coord[0],coord[1]+1,coord[2])]['fill'] =  'wm'
             print('wm key check',dungeon[(coord[0],coord[1]+1,coord[2])])
 
         wm_coord = (coord[0],coord[1]+1,coord[2])
