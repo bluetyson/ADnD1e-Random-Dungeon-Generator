111c8637df584c26e99b8289dc5f136da44dcb68
Blue Tyson <bluetyson@users.noreply.github.com>
2023-01-23 09:50:52 +1030

    key count

--- test_dungeon.py
+++ test_dungeon.py
@@ -3869,7 +3869,7 @@ for down in range(zwidth-1):
                     print("WMKEY:",wandering_monster_stack[wm+1])
                     if isinstance(wandering_monster_stack[wm+1][key]['type'], dict):
                         print("CHARACTER PARTY!")
-                        print(len(wandering_monster_subtable),wandering_monster_subtable)
+                        print("WMSUBTABLE:",len(wandering_monster_subtable),wandering_monster_subtable,"KEYCOUNT:",wandering_monster_stack['key_count'])
                         for c in wandering_monster_stack[wm+1][key]['type']:
                             if 'level' in wandering_monster_stack[wm+1][key]['type'][c]: #hack for a base xp points based on DMG table
                                 cxp = xp_d(wandering_monster_stack[wm+1][key]['type'][c]['level'])
