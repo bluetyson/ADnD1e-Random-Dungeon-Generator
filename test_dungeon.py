984e68b7fef8a39f6ffea09ad82af22fe924e576
Blue Tyson <bluetyson@users.noreply.github.com>
2023-01-22 21:00:21 +1030

    all d first

--- test_dungeon.py
+++ test_dungeon.py
@@ -728,10 +728,13 @@ def check_action(pc_dict, coord, room_stack):
             wandering_monster_stack[wandering_monster_stack['key_count']][wm_coord]['type'] = wm_dict['name']
             wandering_monster_stack[wandering_monster_stack['key_count']][wm_coord]['No'] = wm_dict['no']
 
+            wm_data = all_d[wm_dict['name']]
+            print("WM_DATA1st", wm_data)
+
             if 'Subtable' not in wm_dict['name']:
                 #print(all_d)
                 wm_data = all_d[wm_dict['name']]
-                print("WM_DATA")
+                print("WM_DATA no sub")
                 #print("WM_DATA",wm_data)
                 
 
