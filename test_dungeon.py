8b8502f3cf37ac7138ef708c0c57679af453b2fe
Blue Tyson <bluetyson@users.noreply.github.com>
2023-01-23 13:29:34 +1030

    wm total treasure

--- test_dungeon.py
+++ test_dungeon.py
@@ -3933,14 +3933,14 @@ for down in range(zwidth-1):
             f.write('<br>' + "Wandering Monster Total Treasure:" + str(wm_total_treasure) + '<br>')                            
             f.write('<br>' + "Wandering Monster Subtable:" + str(wandering_monster_subtable) + '<br>')                            
 
-            total_treasure['copper'] = total_treasure['copper'] + treasure['copper']
-            total_treasure['silver'] = total_treasure['silver'] + treasure['silver']
-            total_treasure['electrum'] = total_treasure['electrum'] + treasure['electrum']
-            total_treasure['gold'] = total_treasure['gold'] + treasure['gold']
-            total_treasure['platinum'] = total_treasure['platinum'] + treasure['platinum']
-            total_treasure['gems'] = total_treasure['gems'] + treasure['gems']
-            total_treasure['jewellery'] = total_treasure['jewellery'] + treasure['jewellery']
-            total_treasure['magic'] = total_treasure['magic'] + treasure['magic']            
+            total_treasure['copper'] = total_treasure['copper'] + wm_total_treasure['copper']
+            total_treasure['silver'] = total_treasure['silver'] + wm_total_treasure['silver']
+            total_treasure['electrum'] = total_treasure['electrum'] + wm_total_treasure['electrum']
+            total_treasure['gold'] = total_treasure['gold'] + wm_total_treasure['gold']
+            total_treasure['platinum'] = total_treasure['platinum'] + wm_total_treasure['platinum']
+            total_treasure['gems'] = total_treasure['gems'] + wm_total_treasure['gems']
+            total_treasure['jewellery'] = total_treasure['jewellery'] + wm_total_treasure['jewellery']
+            total_treasure['magic'] = total_treasure['magic'] + wm_total_treasure['magic']            
 
         f.write('<h4>Total Treasure: ' + str(total_treasure) + '</h4>')
         gold = total_treasure['copper'] / 100.0 + total_treasure['silver'] / 10.0 + total_treasure['electrum']/2.0 + total_treasure['gold'] + total_treasure['platinum'] * 10
