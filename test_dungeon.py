2799b57743d54dc48b585349e3f175e589932a72
Blue Tyson <bluetyson@users.noreply.github.com>
2023-01-23 12:31:02 +1030

    dragon subtable

--- test_dungeon.py
+++ test_dungeon.py
@@ -805,10 +805,15 @@ def check_action(pc_dict, coord, room_stack):
                 wandering_monster_subtable.append('character')
                 #quit()
 
+            ###dungeon
+            ###if 'DragonSubtable' in wm_dict['name']:
+               ### wandering_monster_stack[wandering_monster_stack['key_count']][wm_coord]['type'] = wm_dict['details']['name']
+                ###wandering_monster_stack[wandering_monster_stack['key_count']][wm_coord]['No'] = wm_dict['roll'][0]
+
             #if 'DragonSubtable' in wm_dict['name']:
             #if 'Dragon' in wm_dict['type']:                
-            #if 'Dragon' in wm_dict['name']:        
-            if checkdragon:   
+            if 'Dragon' in wm_dict['name']:        
+            #if checkdragon:   
             #if 'DragonSubtable' in wandering_monster_stack[wandering_monster_stack['key_count']][wm_coord]['type']:                    
             #if 'Dragon' in wandering_monster_stack[wandering_monster_stack['key_count']][wm_coord]['type']:                                    
                 print("DragonSubtable")
