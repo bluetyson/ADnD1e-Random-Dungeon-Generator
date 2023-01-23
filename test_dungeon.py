7cb7ce87c1a483005080d10fc19717433bdfa1a5
Blue Tyson <bluetyson@users.noreply.github.com>
2023-01-23 14:38:06 +1030

    magic

--- test_dungeon.py
+++ test_dungeon.py
@@ -3955,7 +3955,7 @@ for down in range(zwidth-1):
                                             monster_treasure['platinum'] = monster_treasure['platinum'] + treasure['platinum']
                                             monster_treasure['gems'] = monster_treasure['gems'] + treasure['gems']
                                             monster_treasure['jewellery'] = monster_treasure['jewellery'] + treasure['jewellery']
-                                            monster_treasure['magic'] = monster_treasure['magic'] + len(treasure['magic'])
+                                            monster_treasure['magic'] = monster_treasure['magic'] + treasure['magic']
 
                                     #do valuations
                                     monster_gems_list = []
