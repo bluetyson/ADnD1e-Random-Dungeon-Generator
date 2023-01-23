7599e09de5de816401cf234dca002ae51545cf49
Blue Tyson <bluetyson@users.noreply.github.com>
2023-01-23 12:06:24 +1030

    dragon check pass

--- test_dungeon.py
+++ test_dungeon.py
@@ -808,14 +808,17 @@ def check_action(pc_dict, coord, room_stack):
                 wandering_monster_stack[wandering_monster_stack['key_count']][wm_coord]['type'] = wm_dict['details']['name']
                 wandering_monster_stack[wandering_monster_stack['key_count']][wm_coord]['No'] = wm_dict['roll'][0]
 
-                dname = wm_dict['details']['name'].split(':')[0]
-                #dname = dname.split('-')[1]
-                print("Dragon dname", dname)
-                wm_data = dragon_d[dname]
-                print("Dragon WM Data", wm_data)
-                wandering_monster_stack[wandering_monster_stack['key_count']][wm_coord]['lair'] = wm_data['lair']
-                wandering_monster_stack[wandering_monster_stack['key_count']][wm_coord]['treasure_individual'] = wm_data['treasure_individual']
-                wandering_monster_stack[wandering_monster_stack['key_count']][wm_coord]['treasure_lair'] = wm_data['treasure_lair']
+                if 1 == 2:
+                    "try and get to dragon quitting
+                    dname = wm_dict['details']['name'].split(':')[0]
+                    #dname = dname.split('-')[1]
+                    print("Dragon dname", dname)
+                    wm_data = dragon_d[dname]
+                    print("Dragon WM Data", wm_data)
+
+                    wandering_monster_stack[wandering_monster_stack['key_count']][wm_coord]['lair'] = wm_data['lair']
+                    wandering_monster_stack[wandering_monster_stack['key_count']][wm_coord]['treasure_individual'] = wm_data['treasure_individual']
+                    wandering_monster_stack[wandering_monster_stack['key_count']][wm_coord]['treasure_lair'] = wm_data['treasure_lair']
 
                 print("dragon quitting")
                 print("DragonSubtableWM:",wandering_monster_stack[wandering_monster_stack['key_count']][wm_coord])
