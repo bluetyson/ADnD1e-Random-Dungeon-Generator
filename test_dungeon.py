e9f932da19a7b2016c69039c848595828e5ac69e
Blue Tyson <bluetyson@users.noreply.github.com>
2023-01-23 15:48:47 +1030

    inlair move

--- test_dungeon.py
+++ test_dungeon.py
@@ -3960,6 +3960,8 @@ for down in range(zwidth-1):
                                 
                                     if l <= lairtry:
                                         inlair = True
+                                        f.write(' is in lair ' + str(inlair))
+                                        
                                 
                                 if inlair:
                                     if len(treasure_list) > 0:
@@ -4019,29 +4021,29 @@ for down in range(zwidth-1):
                                         monster_valuations['magic_xp'].append(m[1][1]) #NOT IMPLEMENTED YET
                                         monster_valuations['treasure']['magic_values'].append(m[1][2]) #NOT IMPLEMENTED YET
 
-                                    if len(treasure_list) > 0:
-                                        if inlair:
-                                            f.write('<h5>Monster Lair Treasure:</h5>')
-                                            f.write(str(l) + ' ' + str(lairtry) + ' ' + str(inlair))
-                                            f.write(str(monster_treasure) + '<br>')
-                                            f.write(str(monster_valuations) + '<br>')
-                                            f.write('<br>')
+                                if len(treasure_list) > 0:
+                                    if inlair:
+                                        f.write('<h5>Monster Lair Treasure:</h5>')
+                                        f.write(str(l) + ' ' + str(lairtry) + ' ' + str(inlair))
+                                        f.write(str(monster_treasure) + '<br>')
+                                        f.write(str(monster_valuations) + '<br>')
+                                        f.write('<br>')
 
-                                        total_treasure_monster['copper'] = total_treasure_monster['copper'] + monster_treasure['copper']
-                                        total_treasure_monster['silver'] = total_treasure_monster['copper'] + monster_treasure['silver']
-                                        total_treasure_monster['electrum'] = total_treasure_monster['electrum'] + monster_treasure['electrum']
-                                        total_treasure_monster['gold'] = total_treasure_monster['gold'] + monster_treasure['gold']
-                                        total_treasure_monster['platinum'] = total_treasure_monster['platinum'] + monster_treasure['platinum']
-                                        total_treasure_monster['gems'] = total_treasure_monster['gems'] + monster_treasure['gems']
-                                        total_treasure_monster['jewellery'] = total_treasure_monster['jewellery'] + monster_treasure['jewellery']
-                                        total_treasure_monster['magic'] = total_treasure_monster['magic'] + monster_treasure['magic']
+                                    total_treasure_monster['copper'] = total_treasure_monster['copper'] + monster_treasure['copper']
+                                    total_treasure_monster['silver'] = total_treasure_monster['copper'] + monster_treasure['silver']
+                                    total_treasure_monster['electrum'] = total_treasure_monster['electrum'] + monster_treasure['electrum']
+                                    total_treasure_monster['gold'] = total_treasure_monster['gold'] + monster_treasure['gold']
+                                    total_treasure_monster['platinum'] = total_treasure_monster['platinum'] + monster_treasure['platinum']
+                                    total_treasure_monster['gems'] = total_treasure_monster['gems'] + monster_treasure['gems']
+                                    total_treasure_monster['jewellery'] = total_treasure_monster['jewellery'] + monster_treasure['jewellery']
+                                    total_treasure_monster['magic'] = total_treasure_monster['magic'] + monster_treasure['magic']
 
-                                        total_valuations_monster['gems'] = total_valuations_monster['gems'] + monster_valuations['gems']
-                                        total_valuations_monster['jewellery'] = total_valuations_monster['jewellery'] + monster_valuations['jewellery']
-                                        total_valuations_monster['gems'] = total_valuations_monster['gems'] + monster_valuations['gems']
-                                        total_valuations_monster['magic_list'] = total_valuations_monster['magic_list'] + monster_valuations['magic_list']
-                                        total_valuations_monster['magic_xp'] = total_valuations_monster['magic_values'] + monster_valuations['magic_xp']
-                                        total_valuations_monster['magic_values'] = total_valuations_monster['magic_values'] + monster_valuations['magic_values']
+                                    total_valuations_monster['gems'] = total_valuations_monster['gems'] + monster_valuations['gems']
+                                    total_valuations_monster['jewellery'] = total_valuations_monster['jewellery'] + monster_valuations['jewellery']
+                                    total_valuations_monster['gems'] = total_valuations_monster['gems'] + monster_valuations['gems']
+                                    total_valuations_monster['magic_list'] = total_valuations_monster['magic_list'] + monster_valuations['magic_list']
+                                    total_valuations_monster['magic_xp'] = total_valuations_monster['magic_values'] + monster_valuations['magic_xp']
+                                    total_valuations_monster['magic_values'] = total_valuations_monster['magic_values'] + monster_valuations['magic_values']
 
 
 
