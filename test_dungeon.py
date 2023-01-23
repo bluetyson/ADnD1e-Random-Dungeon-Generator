40b95c3d117eaf5bdd95a79114843ff68ef86bbc
Blue Tyson <bluetyson@users.noreply.github.com>
2023-01-24 08:54:22 +1030

    take out test functions

--- test_dungeon.py
+++ test_dungeon.py
@@ -3505,15 +3505,8 @@ def level_matrix(level):
 #not checking any level stacks to go up down, similarly
 #not implemented passage width as yet - default only
 
-from monsters import testmonster
-from treasure import testtreasure
-print (testmonster() + ' ' + testtreasure())
-#print(testtreasure())
-
-
 PERIODIC_CHECKS = 1  #number of rolls to make before stopping algorithm  #don't count first one down
 VERBOSITY = 1
-
 
 if len(ARGV) > 1:
     if int(ARGV[1]) > 1:
