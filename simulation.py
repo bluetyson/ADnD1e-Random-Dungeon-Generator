6768ea85a760adf0bcff658c287de4bd348eba70
Blue Tyson <bluetyson@users.noreply.github.com>
2023-01-24 21:16:55 +1030

    simulator start

---
+++ simulation.py
@@ -0,0 +1,22 @@
+
+
+from dungeon_simulation import dungeon_sim
+
+if __name__ == "__main__":
+    PERIODIC_CHECKS = 1
+    VERBOSITY = 0
+
+    if len(ARGV) > 1:
+        if int(ARGV[1]) > 1:
+            PERIODIC_CHECKS = int(ARGV[1])
+
+    if len(ARGV) > 2:
+        VERBOSITY = int(ARGV[2])
+
+    df = dungeon_sim(PERIODIC_CHECKS, VERBOSITY)
+
+    print(df)
+    
+
+
+
