d20cf2e419ed470adec1e48c675b4092fe42de2c
Blue Tyson <bluetyson@users.noreply.github.com>
2023-01-25 11:33:24 +1030

    +1

--- dungeon_simulation.py
+++ dungeon_simulation.py
@@ -4499,7 +4499,7 @@ def dungeon_sim(periodic_checks, verbosity=0, usepath = '', suffix=''):
         for rc in range(room_stack['key_count']):
             if 'contents' in room_stack['shape_dict'][rc+1]:
                 if 'secret_door_dict' in room_stack['shape_dict'][rc]['contents']:
-                    print("\nSECRET DOOR DICT",room_stack['shape_dict'][rc]['contents']['secret_door_dict'])
+                    print("\nSECRET DOOR DICT",room_stack['shape_dict'][rc+1]['contents']['secret_door_dict'])
 
         print("\nDEAD END DICT",dead_end_dict)
         print("\nERROR LOG",error_dict, "WM_STACK:",wandering_monster_stack)
