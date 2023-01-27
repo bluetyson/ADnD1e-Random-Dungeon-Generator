adf40d144d509e1f25fe78324da075138ca7dfa8
Blue Tyson <bluetyson@users.noreply.github.com>
2023-01-27 14:09:46 +1030

    keylist

--- dungeon_simulation.py
+++ dungeon_simulation.py
@@ -4056,6 +4056,7 @@ def dungeon_sim(periodic_checks, verbosity=0, usepath = '', suffix=''):
                     countroom += 1
                     if VERBOSITY:
                         keylist = list(room_stack[countroom].keys())
+                        print(keylist)
                         if room_stack[countroom][keylist[0]][2] == down + 1:
                             f.write('<h4>Data: ' + str(room) + '</h4>')
                             f.write(str(room_stack['shape_dict'][room]) + '<br>')
