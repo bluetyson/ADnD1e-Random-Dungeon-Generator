c786fa48a6f04a0430ee889002197f1221ed568a
Blue Tyson <bluetyson@users.noreply.github.com>
2023-01-27 12:31:48 +1030

    ban

--- dungeon_simulation.py
+++ dungeon_simulation.py
@@ -4506,6 +4506,7 @@ def dungeon_sim(periodic_checks, verbosity=0, usepath = '', suffix=''):
                 
                 url = 'dungeon_' + str(link+1) + '.html'
                 print(url)
+                print(banana)
                 f.write('banana')
                 f.write('<a href="' + url + '"' + '>Level ' + str(link+1) + '</a>' + '<br>')
 
