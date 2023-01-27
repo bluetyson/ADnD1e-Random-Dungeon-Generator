04d2f53ea8ccdf16c78fd62a13012c662277b0ce
Blue Tyson <bluetyson@users.noreply.github.com>
2023-01-27 12:27:00 +1030

    br

--- dungeon_simulation.py
+++ dungeon_simulation.py
@@ -4507,7 +4507,7 @@ def dungeon_sim(periodic_checks, verbosity=0, usepath = '', suffix=''):
                 url = 'dungeon_' + str(link+1) + '.html'
                 print(url)
                 f.write('banana')
-                f.write('<a href="' + url + '"' + '>Level ' + str(link+1) + '</a>' + <br>)
+                f.write('<a href="' + url + '"' + '>Level ' + str(link+1) + '</a>' + '<br>')
 
             str(down+1)
 
