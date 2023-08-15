8311d684bfa4a2b48c977b2e706b35d65c2ae3f3
Blue Tyson <bluetyson@users.noreply.github.com>
2023-08-15 21:42:59 +0930

    try

--- run_simulation.py
+++ run_simulation.py
@@ -4635,14 +4635,14 @@ if __name__ == '__main__':
 
         for future in concurrent.futures.as_completed(future_to_data):
             datainfo = future_to_data[future]
-        #try:
+        try:
             data = future.result()
             result_list.append(data)
             #print("BS", bin_series)
             #if datainfo == '1-2021-10-25-06-21-Invert-the-Gawler-Mira_GeoscienceAnalyst.csv':
-        #except Exception as exc:
+        except Exception as exc:
             print('%r generated an exception: %s' % (datainfo, exc))
-        #else:
+        else:
             print('%r run finished' % (datainfo))
 
     #print(bin_series)
